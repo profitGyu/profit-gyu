a7f5a87a8268f4fbbd095c8da9c4dfc5ac39487f
profitgyu <benefitgyu@luvd.co.kr>
2025-01-06 17:41:35 +0900

    chore: 사용하지 않는 파일 삭제

--- src/services/_axios/httpWebClient.ts
+++ src/services/_axios/httpWebClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import type { IBaseResponse } from './type';
 import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { getToken, removeToken, saveToken } from '@utils/common/auth';
+
 import { ROUTES } from 'routes';
 
 const REQUEST_TIMEOUT = 20000;
@@ -15,10 +15,6 @@ export const instance = axios.create({
 // Request Interceptor
 instance.interceptors.request.use(
   (config) => {
-    const token = getToken();
-    if (token?.access_token) {
-      config.headers.Authorization = `Bearer ${token.access_token}`;
-    }
     return config;
   },
   (error) => {
@@ -37,47 +33,10 @@ instance.interceptors.response.use(
       (error.response?.status === 401 || error.response?.status === 403) &&
       originalRequest
     ) {
-      const token = getToken();
-      // if (!token?.refresh_token) {
-      //   removeToken();
-      //   window.location.href = ROUTES.AUTH.login;
-      //   return Promise.reject(error);
-      // }
-
-      try {
-        // 리프레시 토큰으로 새로운 액세스 토큰 발급 요청
-        const response = await axios.post(
-          `${process.env.REACT_APP_BASE_API}/account/token/refresh`,
-          { refresh_token: token?.refresh_token },
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Authorization: `Bearer ${token?.refresh_token}`,
-            },
-          },
-        );
-
-        const { access_token, refresh_token } = response.data;
-
-        // 새로운 토큰 저장
-        saveToken({ access_token, refresh_token });
-
-        // 실패했던 요청 재시도
-        if (originalRequest.headers) {
-          originalRequest.headers.Authorization = `Bearer ${access_token}`;
-        }
-        return axios(originalRequest);
-      } catch (refreshError) {
-        // 리프레시 토큰도 만료된 경우
-        // removeToken();
-        // window.location.href = ROUTES.AUTH.login;
-        return Promise.reject(refreshError);
+      if (error.response?.data) {
+        // 기타 에러 처리
+        return Promise.reject(error.response.data);
       }
-    }
-
-    // 기타 에러 처리
-    if (error.response?.data) {
-      return Promise.reject(error.response.data);
     }
     return Promise.reject(error);
   },
